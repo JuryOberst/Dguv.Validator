2e006a3adbf2439f633444c94b009c63f494f298
Mark Junker <m.junker@fubar-dev.de>
2015-05-07 14:30:34 +0200

    Verschoben und angepasst, damit die NuGet-Funktionen von TeamCity genutzt werden können. Die PowerShell-Scripte werden nicht mehr benötigt.

--- nuget-pack.ps1
+++
@@ -1,9 +0,0 @@
-[OutputType([void])]
-param(
-	[Parameter()]
-	$version = "1.0.1",
-	[Parameter()]
-	$config = "Release"
-)
-
-& nuget pack .\NuGet\Dguv.Validator.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
--- nuget-push.ps1
+++
@@ -1,9 +0,0 @@
-[OutputType([void])]
-param(
-	[Parameter()]
-	$apiKey,
-	[Parameter()]
-	$version = "1.0.1"
-)
-
-& nuget push -ApiKey "$apiKey" .\Dataline.Dguv.Validator.$version.nupkg
